b0374e9b1af215a01a0af8bcf651b7da415f0a51
srdjangojkov <srdjaliman@gmail.com>
2021-10-06 19:58:25 +0200

    Finish script refactoring

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-$(window).on("scroll", function () {
+$(window).on('scroll', function () {
 	if ($(window).scrollTop() > 50) {
-		$(".header").addClass("active");
+		$('.header').addClass('active');
 	} else {
-		$(".header").removeClass("active");
+		$('.header').removeClass('active');
 	}
 });
-let vid = document.getElementById("teaser-vid");
+
 function playVideo() {
+	const vid = document.getElementById('teaser-vid');
 	vid.play();
 }
+
 function isValidEmail(email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var re = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
-document.getElementById("card-1").addEventListener("click", myFunction1);
-document.getElementById("card-2").addEventListener("click", myFunction2);
-document.getElementById("card-3").addEventListener("click", myFunction3);
-document.getElementById("percentage").addEventListener("click", myFunction4);
-function myFunction1() {
-	document.getElementById("card-1").innerHTML = "Ocena: ☆☆☆☆☆";
+
+function showRating(el) {
+	el.innerText = 'Ocena: ☆☆☆☆☆';
 }
-function myFunction2() {
-	document.getElementById("card-2").innerHTML = "Ocena: ☆☆☆☆☆";
-}
-function myFunction3() {
-	document.getElementById("card-3").innerHTML = "Ocena: ☆☆☆☆";
-}
-function myFunction4() {
-	document.getElementById("percentage").innerHTML = "Vizuelno zapanjujući i narativno zadovoljavajući, Blade Runner 2049 produbljuje i proširuje priču svog prethodnika dok uspeva i kao samostalno impresivno filmsko dostignuće.";
+
+function toggleReview() {
+	let el = document.getElementById('percentage');
+	let text = el.innerText;
+	let review = 'Vizuelno zapanjujući i narativno zadovoljavajući, Blade Runner 2049 produbljuje i proširuje priču svog prethodnika dok uspeva i kao samostalno impresivno filmsko dostignuće.';
+	el.innerText = text == review ? 'Ocena: ☆☆☆☆☆' : review;
 }
